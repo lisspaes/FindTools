13498d0687ce067b1c0213f8b95d4e066f716a91
lss <lissete.paes@gmail.com>
2022-04-03 19:33:24 -0500

    Agregar popup de Reseña a la vista de "Mi perfil"

--- lib/screens/perfilUsuario.dart
+++ lib/screens/perfilUsuario.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
 
 class perfilUsuario extends StatefulWidget{
   @override
@@ -8,58 +10,59 @@ class perfilUsuario extends StatefulWidget{
 class _perfilUsuarioState extends State<perfilUsuario> {
   @override
   Widget build(BuildContext context){
-    return Padding(
+    return ListView(
       padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          Card(
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          children: [
+            Card(
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
 
-                children: [
-                  Column(
-                    children: [
-                      const Text(
-                        '@Usuario',
-                        style: TextStyle(fontSize: 25),
-
-                      ),
-                      const Text(
-                        'Cuautla, Mor.',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                    ],
-                  ),
-                  const Image(
-                    image: NetworkImage('https://www.sircuenca.com/images/admin.png'),
-                    height: 100,
-                  ),
-                ],
-              ),
-            ),
-
-          ),
-          Container(
-          height: 250,
-            child: Card(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 50,
-                    child: Text(
-                      "Servicios en proceso",
-                      style: TextStyle( fontSize: 20, color: Colors.black54),
-                      textAlign: TextAlign.justify,
-                    ),
-                  ),
-                  Container(
-                    height: 180,
-                    child: ListView(
-                      scrollDirection: Axis.horizontal,
+                  children: [
+                    Column(
                       children: [
-                        Padding(
+                        const Text(
+                          '@Usuario',
+                          style: TextStyle(fontSize: 25),
+
+                        ),
+                        const Text(
+                          'Cuautla, Mor.',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ],
+                    ),
+                    const Image(
+                      image: NetworkImage('https://www.sircuenca.com/images/admin.png'),
+                      height: 100,
+                    ),
+                  ],
+                ),
+              ),
+
+            ),
+            Container(
+              height: 250,
+              child: Card(
+                child: Column(
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      child: Text(
+                        "Servicios en proceso",
+                        style: TextStyle( fontSize: 20, color: Colors.black54),
+                        textAlign: TextAlign.justify,
+                      ),
+                    ),
+                    Container(
+                      height: 180,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          Padding(
                             padding: const EdgeInsets.all(8.0),
                             child: Container(
                               width: 100,
@@ -86,94 +89,94 @@ class _perfilUsuarioState extends State<perfilUsuario> {
                                 ],
                               ),
                             ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 100,
-                            decoration: BoxDecoration(color: Colors.white70),
-                            child: Column(
-                              children: [
-                                const Image(
-                                  image: NetworkImage('https://web.hometask.mx/wp-content/uploads/2021/07/pintor.jpg'),
-                                ),
-                                TextButton(onPressed: (){print('Seleccionar');},
-                                    child: Text("Mostrar status",
-                                      style: TextStyle(
-                                        color: Colors.indigo,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: 100,
+                              decoration: BoxDecoration(color: Colors.white70),
+                              child: Column(
+                                children: [
+                                  const Image(
+                                    image: NetworkImage('https://web.hometask.mx/wp-content/uploads/2021/07/pintor.jpg'),
+                                  ),
+                                  TextButton(onPressed: (){print('Seleccionar');},
+                                      child: Text("Mostrar status",
+                                        style: TextStyle(
+                                          color: Colors.indigo,
 
-                                        fontSize: 10,
-                                      ),))
-
-
+                                          fontSize: 10,
+                                        ),))
 
 
-                              ],
+
+
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 100,
-                            decoration: BoxDecoration(color: Colors.white70),
-                            child: Column(
-                              children: [
-                                const Image(
-                                  image: NetworkImage('https://web.hometask.mx/wp-content/uploads/2021/07/electricista.jpg'),
-                                ),
-                                TextButton(onPressed: (){print('Seleccionar');},
-                                    child: Text("Mostrar status",
-                                      style: TextStyle(
-                                        color: Colors.indigo,
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: 100,
+                              decoration: BoxDecoration(color: Colors.white70),
+                              child: Column(
+                                children: [
+                                  const Image(
+                                    image: NetworkImage('https://web.hometask.mx/wp-content/uploads/2021/07/electricista.jpg'),
+                                  ),
+                                  TextButton(onPressed: (){print('Seleccionar');},
+                                      child: Text("Mostrar status",
+                                        style: TextStyle(
+                                          color: Colors.indigo,
 
-                                        fontSize: 10,
-                                      ),))
-
+                                          fontSize: 10,
+                                        ),))
 
 
 
-                              ],
+
+                                ],
+                              ),
                             ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Container(
-                            width: 100,
-                            decoration: BoxDecoration(color: Colors.white70),
-                            child: Column(
-                              children: [
-                                const Image(
-                                  image: NetworkImage('https://web.hometask.mx/wp-content/uploads/2021/07/plomero.jpg'),
-                                ),
-                                TextButton(onPressed: (){print('Seleccionar');},
-                                    child: Text("Mostrar status",
-                                      style: TextStyle(
-                                        color: Colors.indigo,
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              width: 100,
+                              decoration: BoxDecoration(color: Colors.white70),
+                              child: Column(
+                                children: [
+                                  const Image(
+                                    image: NetworkImage('https://web.hometask.mx/wp-content/uploads/2021/07/plomero.jpg'),
+                                  ),
+                                  TextButton(onPressed: (){print('Seleccionar');},
+                                      child: Text("Mostrar status",
+                                        style: TextStyle(
+                                          color: Colors.indigo,
 
-                                        fontSize: 10,
-                                      ),))
-
+                                          fontSize: 10,
+                                        ),))
 
 
 
-                              ],
+
+                                ],
+                              ),
                             ),
                           ),
-                        ),
 
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
 
             ),
 
-          Container(
-            height: 250,
+            Container(
+              height: 250,
 
               child: Card(
                 child: Column(
@@ -202,26 +205,29 @@ class _perfilUsuarioState extends State<perfilUsuario> {
                                   const Image(
                                     image: NetworkImage('https://web.hometask.mx/wp-content/uploads/2021/07/fumigador.jpg'),
                                   ),
-                                  TextButton(onPressed: (){print('Seleccionar');},
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                        crossAxisAlignment: CrossAxisAlignment.center,
-                                        children: [
-                                          Text("Califícanos",
-                                        style: TextStyle(
-                                          color: Colors.indigo,
-
-                                          fontSize: 10,),
-                                          ),
-                                          Icon(
-                                            Icons.star_border_outlined,
+                                  TextButton(
+                                    onPressed: (){
+                                      abrirCalificacion();
+                                    },
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: [
+                                        Text("Califícanos",
+                                          style: TextStyle(
                                             color: Colors.indigo,
-                                            size: 15,
-                                          )
 
-                                        ],
-
+                                            fontSize: 10,),
                                         ),
+                                        Icon(
+                                          Icons.star_border_outlined,
+                                          color: Colors.indigo,
+                                          size: 15,
+                                        )
+
+                                      ],
+
+                                    ),
                                   )
                                 ],
                               ),
@@ -274,8 +280,10 @@ class _perfilUsuarioState extends State<perfilUsuario> {
               ),
             ),
 
-        ],
-      ),
+          ],
+        ),
+      ],
+
     );
 
 
@@ -284,13 +292,8 @@ class _perfilUsuarioState extends State<perfilUsuario> {
       context: context,
       builder: (context)=>AlertDialog(
         title: Text("Status del servicio"),
-        content: (
-            SizedBox(
-                height:200,
-                width: 300,
-                child:
-                Image.asset('assets/images/progreso2.png')
-            )
+        content: Image(
+          image: AssetImage("assets/images/progreso2.png"),
         ),
         actions: [
           TextButton(
@@ -304,4 +307,54 @@ class _perfilUsuarioState extends State<perfilUsuario> {
       void submit(){
         Navigator.of(context).pop();
       }
+
+  Future abrirCalificacion()=>showDialog(
+    context: context,
+    builder: (context)=>AlertDialog(
+      title: Text("¡Califícanos!"),
+      content: Container(
+        height: 150,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            RatingBar.builder(
+              initialRating: 1,
+              minRating: 1,
+              direction: Axis.horizontal,
+              allowHalfRating: false,
+              itemCount: 5,
+              itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+              itemBuilder: (context, _) => Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              onRatingUpdate: (rating) {
+                print(rating);
+              },
+            ),
+            Flexible(
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Danos tu opinión",
+                ),
+                maxLines: 4,
+                keyboardType: TextInputType.multiline,
+                maxLength: 100,
+              ),
+            )
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          child: Text('Cerrar'),
+          onPressed: cerrar,
+        )
+      ],
+    ),
+  );
+
+  void cerrar(){
+    Navigator.of(context).pop();
+  }
 }
